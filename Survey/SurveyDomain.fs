072ceddb9e8496bb488d7ee8ecd4a1fdbf996c7c
Mark Cheeseman <mcheeseman@users.noreply.github.com>
2016-07-05 21:20:18 +1000

    Add domain logic

--- Survey/SurveyDomain.fs
+++ Survey/SurveyDomain.fs
@@ -2,11 +2,19 @@
 
 module SurveyDomain = 
     open System
+    open Hombredequeso.Rop
 
     module Question =
+        type Error =
+            | TooShort
+            | TooLong
+
         type T = Question of string
         let create (s: string) =
-            Question s
+            match s with
+            | _ when s.Length < 4 -> TooShort |> fail
+            | _ when s.Length > 512 -> TooLong |> fail
+            | _ -> Question s |> succeed
         let value (Question q) = q
 
     type SurveyQuestion = {
@@ -23,15 +31,51 @@ module SurveyDomain =
         Sections: Section list
     }
 
-    //let getSection (survey: Survey) (question: SurveyQuestion) 
-    //        : Section option =
-    //    None 
+    type Answer = {
+        id: Guid
+        result: bool
+    }
 
-    //type GetNextQuestionResult =
-    //    | Question of SurveyQuestion
-    //    | EndOfSurvey
-    //    | QuestionNotInSurvey
+    type NoNextResult =
+        | SurveyComplete
+        | SurveyIncompleteRequiringChangesToProgress
+        | ErrorNoSurveyQuestions
 
-    //let getNextQuestion (survey: Survey) (lastQuestionAnswered: Guid) (answer: bool) : GetNextQuestionResult =
-    //    EndOfSurvey
+    // surveyIndex*questionIndex*SurveyQuestion
+    let flatten (s:Survey): (int*int*SurveyQuestion) list =
+        let indexItems i s = i,s
+        let iSections = s.Sections |> List.mapi indexItems
+        let getNewAcc acc ((sectioni, s):int*Section) = 
+            let iQuestions = s.Questions |> List.mapi (fun i s -> sectioni, i, s)
+            acc @ iQuestions 
+        let iiAnswer = List.fold (fun acc e -> getNewAcc acc e) [] iSections
+        iiAnswer
+
+    // surveyIndex*questionIndex*SurveyQuestion*Answer option
+    let correlate (questions: (int*int*SurveyQuestion)list) (answers: Answer list) =
+       let getAnswer id = answers |> List.tryFind (fun a -> a.id = id) |> Option.map (fun a -> a.result)
+       questions |> List.map (fun (sI, qI, q) -> (sI, qI, q, (getAnswer q.id)))
+
+    let rec getRemainingFromNextSection 
+            (x: (int*int*SurveyQuestion*bool option)list) (currentSection: int) =
+        match x with
+            | [] -> []
+            | (iSection,_,_,_)::remaining when iSection = currentSection -> getRemainingFromNextSection remaining currentSection
+            | (iSection,_,_,_)::remaining (* when iSection <> currentSection *)-> x
+
+    let rec getNextInner (x: (int*int*SurveyQuestion*bool option)list) : RopResult<SurveyQuestion, NoNextResult> =
+        match x with
+            | [] -> SurveyComplete |> fail
+            | (_,_,q,None)::_ -> q |> succeed
+            | (iSection,_,q,Some false)::remainingQuestions -> getNextInner (getRemainingFromNextSection remainingQuestions iSection)
+            | (_,_,q,Some true)::remainingQuestions-> getNextInner remainingQuestions
+
+    let getNext (s:Survey) (answers: Answer list) : RopResult<SurveyQuestion, NoNextResult> =
+        let qns = flatten s
+        let qAndA = correlate qns answers
+
+        match qAndA with
+            | [] -> ErrorNoSurveyQuestions |> fail
+            | x -> getNextInner x
+
 
--- SurveyTests/SurveyDomainTests.fs
+++ SurveyTests/SurveyDomainTests.fs
@@ -3,11 +3,150 @@
 open NUnit.Framework
 open System
 open FsUnit
+open System
+open Survey.SurveyDomain
+open Hombredequeso.Rop
 
 [<TestFixture>]
-type SurveyDomainTests() =
+module SurveyDomainTests =
+    
+    let take n l = l 
+                    |> List.mapi (fun i x -> (i,x)) 
+                    |> List.filter (fun (i,x) -> i < n)
+                    |> List.map (fun (i,x) -> x)
 
+    let idList = [
+        Guid.Parse("00000000-C05A-418A-A320-19B3F770A3B1");
+        Guid.Parse("00000001-C05A-418A-A320-19B3F770A3B1");
+        Guid.Parse("00000002-C05A-418A-A320-19B3F770A3B1");
+        Guid.Parse("00000003-C05A-418A-A320-19B3F770A3B1");
+        Guid.Parse("00000004-C05A-418A-A320-19B3F770A3B1");
+        Guid.Parse("00000005-C05A-418A-A320-19B3F770A3B1");
+        Guid.Parse("00000006-C05A-418A-A320-19B3F770A3B1");
+        Guid.Parse("00000007-C05A-418A-A320-19B3F770A3B1");
+    ]
+
+    let sampleSurvey: Survey = {
+        id = Guid.NewGuid()
+        Sections = [
+                        {
+                            Questions = [
+                                            {
+                                                id = idList.[0]
+                                                question = Question.Question "question0"
+                                            };
+                                            {
+                                                id = idList.[1]
+                                                question = Question.Question "question1"
+                                            };
+                                            {
+                                                id = idList.[2]
+                                                question = Question.Question "question2"
+                                            };
+                                        ]
+                        };
+                        {
+                            Questions = [
+                                            {
+                                                id = idList.[3]
+                                                question = Question.Question "question3"
+                                            };
+                                            {
+                                                id = idList.[4]
+                                                question = Question.Question "question4"
+                                            };
+                                        ]
+                        };
+                        {
+                            Questions = [
+                                            {
+                                                id = idList.[5]
+                                                question = Question.Question "question5"
+                                            };
+                                        ]
+                        };
+                        {
+                            Questions = [
+                                            {
+                                                id = idList.[6]
+                                                question = Question.Question "question6"
+                                            };
+                                            {
+                                                id = idList.[7]
+                                                question = Question.Question "question7"
+                                            };
+                                        ]
+                        };
+        ]
+    }
 
     [<Test>]
-    member public x.``Basic Test`` () =
-        1 |> should equal 1
+    let ``Valid question returns a succeed`` () =
+        let q = "Why did the chicken cross the road?"
+        let question = Question.create q
+
+        match question with
+            | Success (qn,_) ->
+                                Question.value qn |> should equal q
+            | Failure [] -> Assert.Fail()
+            | Failure (h::t) -> Assert.Fail()
+
+    [<Test>]
+    let ``InValid question too short returns a fail`` () =
+        let q = "Why"
+        let question: RopResult<Question.T, Question.Error> = Question.create q
+
+        let expectedFailure = [Question.Error.TooShort]
+        match question with
+            | Failure [x] when x = Question.Error.TooShort -> Assert.Pass()
+            | _ -> Assert.Fail()
+
+    let createRandomSurveyQuestion id x =
+        let q = Question.Question ("abc" + x.ToString() + "." + id.ToString())
+        {
+            id = id
+            question = q
+        }
+
+    [<Test>]
+    let ``getNext returns ErrorNoSurveyQuestions when there are no survey questions`` () =
+        let nextQuestion = getNext sampleSurvey []
+        let expectedResult:RopResult<SurveyQuestion, NoNextResult> = sampleSurvey.Sections.[0].Questions.[0] |> succeed
+        nextQuestion |> should equal expectedResult
+
+    [<Test>]
+    let ``getNext returns first question where no Answers`` () =
+        let nextQuestion = getNext sampleSurvey []
+        let expectedResult:RopResult<SurveyQuestion, NoNextResult> = sampleSurvey.Sections.[0].Questions.[0] |> succeed
+        nextQuestion |> should equal expectedResult
+
+    [<Test>]
+    let ``getNext returns SurveyComplete if all questions answered`` () =
+        let allAnswers = idList|> List.map (fun x -> {id=x; result=true})
+
+        let nextQuestion = getNext sampleSurvey allAnswers
+
+        match nextQuestion with
+            | Failure [SurveyComplete]  -> Assert.Pass()
+            | _ -> Assert.Fail()
+
+    [<Test>]
+    let ``getNext returns next question after a true answer`` () =
+
+        let firstAnswerOnly = idList |> take 1 |> List.map (fun x -> {id=x;result=true})
+
+        let nextQuestion = getNext sampleSurvey firstAnswerOnly
+
+        match nextQuestion with
+            | Success (secondQuestion, _)  -> secondQuestion |> should equal sampleSurvey.Sections.[0].Questions.[1]
+            | _ -> Assert.Fail()
+
+    [<Test>]
+    let ``getNext returns next section first question after a false answer`` () =
+        let firstAnswerOnly = idList |> take 1 |> List.map (fun x -> {id=x;result=false})
+
+        let nextQuestion = getNext sampleSurvey firstAnswerOnly
+
+        match nextQuestion with
+            | Success (secondSectionFirstQuestion, _)  -> secondSectionFirstQuestion |> should equal sampleSurvey.Sections.[1].Questions.[0]
+            | _ -> Assert.Fail()
